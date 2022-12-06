4e093d35023d28cb0b539ad45fa29cbdd168affb
Eric Opoku <eopoku@eopoku01.mynetworksettings.com>
2022-12-05 23:20:42 -0500

    downgrade java to 17

--- src/main/java/com/developersboard/shared/impl/DefaultTag.java
+++ src/main/java/com/developersboard/shared/impl/DefaultTag.java
@@ -2,24 +2,64 @@ package com.developersboard.shared.impl;
 
 import com.developersboard.shared.Tag;
 import com.developersboard.shared.TaggedFile;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.locks.StampedLock;
 
-@Data
 @Component
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class DefaultTag implements Tag {
 
-    @EqualsAndHashCode.Include
     private String name;
 
     private final StampedLock lock = new StampedLock();
 
     private final Set<TaggedFile> files = new HashSet<>();
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public StampedLock getLock() {
+        return this.lock;
+    }
+
+    @Override
+    public Collection<TaggedFile> getFiles() {
+        return this.files;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        var that = (DefaultTag) o;
+
+        return getName().equals(that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+
+    @Override
+    public String toString() {
+        return "DefaultTag{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/developersboard/shared/impl/DefaultTaggedFile.java
+++ src/main/java/com/developersboard/shared/impl/DefaultTaggedFile.java
@@ -2,9 +2,6 @@ package com.developersboard.shared.impl;
 
 import com.developersboard.shared.Tag;
 import com.developersboard.shared.TaggedFile;
-import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 import java.nio.file.Path;
 import java.util.Collection;
@@ -12,16 +9,28 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.locks.StampedLock;
 
-@Data
-@RequiredArgsConstructor
 public class DefaultTaggedFile implements TaggedFile {
 
-    @NonNull private final Path path;
+    private final Path path;
     private final Set<Tag> tags = new HashSet<>();
     private final StampedLock lock = new StampedLock();
+
+    public DefaultTaggedFile(Path path) {
+        this.path = path;
+    }
 
     @Override
     public String getName() {
         return this.path.toString();
+    }
+
+    @Override
+    public StampedLock getLock() {
+        return this.lock;
+    }
+
+    @Override
+    public Collection<Tag> getTags() {
+        return this.tags;
     }
 }
